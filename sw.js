911340e85dad1d51c57d7ea4069844ffd4fb4f94
sharifmb92-bit <sharif.mb92@gmail.com>
2026-04-29 22:03:13 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const cacheName = 'mcash-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
